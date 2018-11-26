9fd432f0e3665a3150880dc40eb5a958fea051eb
Itamar Faran <itamarf@wix.com>
2018-11-26 15:35:08 +0200

    finished bootstraps and plots

--- Main Work/Code/bootstraps2.R
+++ Main Work/Code/bootstraps2.R
@@ -12,18 +12,18 @@ if(ncores > 1) requiredFunction <- c("Estimate.Loop", "Estimate.Loop2",
 Tlength <- 115
 B <- 100
 p <- 10
-sampleData <- createSamples(B = B, nH = 107, nS = 92, p = p, Tlength = Tlength,
+sampleDataB <- createSamples(B = B, nH = 107, nS = 92, p = p, Tlength = Tlength,
                             percent_alpha = 0.4, range_alpha = c(0.6, 0.8))
 
 
 bootstrapFunction <- function(b){
-  res_unspecified <- Estimate.Loop(Healthy_List = sampleData$samples[[b]]$healthy,
-                                   Sick_List = sampleData$samples[[b]]$sick)
+  res_unspecified <- Estimate.Loop(Healthy_List = sampleDataB$samples[[b]]$healthy,
+                                   Sick_List = sampleDataB$samples[[b]]$sick)
   
   res_specified <- Estimate.Loop2(theta0 = res_unspecified$theta,
                                   alpha0 = res_unspecified$alpha,
-                                  healthy.data = sampleData$samples[[b]]$healthy,
-                                  sick.data = sampleData$samples[[b]]$sick,
+                                  healthy.data = sampleDataB$samples[[b]]$healthy,
+                                  sick.data = sampleDataB$samples[[b]]$sick,
                                   T_thresh = 10^4, progress = FALSE)
   return(res_specified)
 }
@@ -31,7 +31,7 @@ bootstrapFunction <- function(b){
 tt1 <- Sys.time()
 if(ncores > 1){
   buildCL(ncores, c("dplyr", "matrixcalc"), requiredFunction)
-  clusterExport(cl = cl, "sampleData")
+  clusterExport(cl = cl, "sampleDataB")
   simuldat <- parLapply(cl = cl, 1:B, bootstrapFunction)
   stopCluster(cl = cl)
 } else {
@@ -45,8 +45,8 @@ for(b in 1:B){
   alpha_simul[b,] <- simuldat[[b]]$alpha
   estN_all[b] <- simuldat[[b]]$Est_N
 } 
-VarAlphaByHess <- ComputeFisher(simuldat[[1]], sampleData$samples[[1]]$sick, "Hess") %>% solve
-VarAlphaByGrad <- ComputeFisher(simuldat[[1]], sampleData$samples[[1]]$sick, "Grad") %>% solve
+VarAlphaByHess <- ComputeFisher(simuldat[[1]], sampleDataB$samples[[1]]$sick, "Hess") %>% solve
+VarAlphaByGrad <- ComputeFisher(simuldat[[1]], sampleDataB$samples[[1]]$sick, "Grad") %>% solve
 VarAlphaCombined <- VarAlphaByHess %*% solve(VarAlphaByGrad) %*% VarAlphaByHess
 
 Emp_vs_Theo <- data.frame(TheoreticHess = sqrt(diag(VarAlphaByHess)),
@@ -73,22 +73,22 @@ B <- 100
 Tlist <- c(10, 30, 50, 70, 100, 120, 150, 170, 200, 250, 300, 400, 700, 1000, 1500, 2000, 3000, 4000)
 lngth_Tlist <- length(Tlist)
 seed <- sample(1:10000, 3)
-sampleDataT <- list()
+sampleDataBT <- list()
 
 for(t in 1:lngth_Tlist){
-  sampleDataT[[t]] <- createSamples(B = B, nH = 107, nS = 92, p = p, Tlength = Tlist[t],
+  sampleDataBT[[t]] <- createSamples(B = B, nH = 107, nS = 92, p = p, Tlength = Tlist[t],
                                     percent_alpha = 0.4, range_alpha = c(0.6, 0.8), seed = seed) %>%
     append(c("Tlength" = Tlist[t]), 0)
 }
 
 bootstrapFunction <- function(b, k){
-  res_unspecified <- Estimate.Loop(Healthy_List = sampleDataT[[k]]$samples[[b]]$healthy,
-                                   Sick_List = sampleDataT[[k]]$samples[[b]]$sick)
+  res_unspecified <- Estimate.Loop(Healthy_List = sampleDataBT[[k]]$samples[[b]]$healthy,
+                                   Sick_List = sampleDataBT[[k]]$samples[[b]]$sick)
   
   res_specified <- Estimate.Loop2(theta0 = res_unspecified$theta,
                                   alpha0 = res_unspecified$alpha,
-                                  healthy.data = sampleDataT[[k]]$samples[[b]]$healthy,
-                                  sick.data = sampleDataT[[k]]$samples[[b]]$sick,
+                                  healthy.data = sampleDataBT[[k]]$samples[[b]]$healthy,
+                                  sick.data = sampleDataBT[[k]]$samples[[b]]$sick,
                                   T_thresh = 10^4, progress = FALSE)
   return(res_specified)
 }
@@ -97,7 +97,7 @@ simuldatT <- list()
 tt2 <- Sys.time()
 if(ncores > 1){
   buildCL(ncores, c("dplyr", "matrixcalc"), requiredFunction)
-  clusterExport(cl = cl, "sampleDataT")
+  clusterExport(cl = cl, "sampleDataBT")
   for(t in 1:lngth_Tlist){
     cat(paste0(Tlist[t], " (", round(100*t/lngth_Tlist), "%); "))
     clusterExport(cl = cl, "t")
@@ -123,8 +123,8 @@ for(t in 1:lngth_Tlist){
     alpha_simul[b,,t] <- simuldatT[[t]][[b]]$alpha
     estNT_all[b,t] <- simuldatT[[t]][[b]]$Est_N
   }
-  tmpG <- ComputeFisher(simuldatT[[t]][[1]], sampleDataT[[t]]$samples[[1]]$sick, "Grad") %>% solve
-  tmpH <- ComputeFisher(simuldatT[[t]][[1]], sampleDataT[[t]]$samples[[1]]$sick, "Hess") %>% solve
+  tmpG <- ComputeFisher(simuldatT[[t]][[1]], sampleDataBT[[t]]$samples[[1]]$sick, "Grad") %>% solve
+  tmpH <- ComputeFisher(simuldatT[[t]][[1]], sampleDataBT[[t]]$samples[[1]]$sick, "Hess") %>% solve
   tmpC <- tmpH %*% solve(tmpG) %*% tmpH
   
   alpha_sdGrad[t,] <- sqrt(diag(tmpG))

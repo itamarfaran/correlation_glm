c02a9de54d68cec31eee17fa42a585188a3cae50
itamar.faran <itamar.faran@gmail.com>
2018-12-30 01:55:46 +0200

    removed NULL in some functions of file 02

--- main_work/Code/02_simulationFunctions.R
+++ main_work/Code/02_simulationFunctions.R
@@ -1,30 +1,30 @@
-build_parameters <- function(p, percent_alpha, range_alpha, loc_scale = c(0,1), seed = NULL){
+build_parameters <- function(p, percent_alpha, range_alpha, loc_scale = c(0,1), seed){
   #Build Real Sigma and Theta
-  if(length(seed) > 0) set.seed(seed[1])
+  if(!missing(seed)) set.seed(seed[1])
   temp <- matrix(rnorm(2*p^2, loc_scale[1], loc_scale[2]), nrow = 2*p)
   temp <- t(temp)%*%temp
   real.theta <- force_symmetry(cov2cor(temp))
   
   #Generate Variances and create Variance matrix parameters
-  if(length(seed) > 0) set.seed(seed[2])
+  if(!missing(seed)) set.seed(seed[2])
   varss <- rlogis(p)^2
   real.sigma <- sqrt(diag(varss)) %*% real.theta %*% sqrt(diag(varss))
   
   #Build Real Alpha
   alpha <- rep(1,p)
-  if(length(seed) > 0) set.seed(seed[3])
+  if(!missing(seed)) set.seed(seed[3])
   alpha[sample(1:p, floor(percent_alpha * p))] <- runif(floor(percent_alpha * p), range_alpha[1], range_alpha[2])
   
   return(list(Corr.mat = real.theta, Cov.mat = real.sigma, Alpha = alpha))
 }
 
-create_correlation_matrices <- function(real_corr, sample_size, df = 0, AR = NULL, MA = NULL, var_scale = NULL,
-                                        seed.control = NULL, silent = FALSE){
+create_correlation_matrices <- function(real_corr, sample_size, df = 0, AR = NULL, MA = NULL, var_scale,
+                                        seed.control, silent = FALSE){
   if(!is.positive.definite(real_corr)) stop("real_corr not positive definite")
   p <- nrow(real_corr)
   if((df > 0) & (df <= p + 1)) warning("df <= p + 1, p + 1 used in wishart simulations")
   df <- max(df, p + 1)
-  if(length(var_scale)==0){
+  if(missing(var_scale)){
     var_scale <- runif(p,10,100)
   } else if(length(var_scale)!=p){
     stop("var_scale not in dimension")
@@ -34,10 +34,10 @@ create_correlation_matrices <- function(real_corr, sample_size, df = 0, AR = NUL
   real_var <- Dhalf%*%real_corr%*%Dhalf
   
   if(is.null(AR) & is.null(MA)){
-    if(length(seed.control) == 1) set.seed(seed.control)
+    if(!missing(seed.control)) set.seed(seed.control)
     pelet_matrices <- rWishart(sample_size, df, real_var)
   } else {
-    if(length(seed.control) == 1) stop("seed.control not available for rWishart_ARMA")
+    if(!missing(seed.control)) stop("seed.control not available for rWishart_ARMA")
     pelet_matrices <- rWishart_ARMA(sample_size, df, real_var, AR = AR, MA = MA, silent = silent)
   }
   for(b in 1:sample_size) pelet_matrices[,,b] <- force_symmetry(cov2cor(pelet_matrices[,,b]))

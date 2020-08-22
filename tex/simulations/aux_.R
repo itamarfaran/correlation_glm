c8275c42e3d720956560dae88d4c7958c6a7e87e
itamarf <itamar.faran@gmail.com>
2020-08-22 20:56:36 +0300

    refactor functions in aux_ file

--- tex/simulations/aux_.R
+++ tex/simulations/aux_.R
@@ -12,7 +12,71 @@ custom_ggsave <- function(filename, plot, width = 1, height = 1, ...){
   ggsave(filename = filename, plot = plot, path = plot_files_path,
          width = width*10, height = height*10, units = 'cm', dpi = dpi, ...)
 }
-  
+
+ggsave_batch <- function(l_plots, names, dir = 'temp'){
+  if(!dir.exists(dir)) dir.create(dir)
+  for(i in seq_along(l_plots)) ggsave(paste0(dir, '/', names[i], '.png'), l_plots[[i]])
+}
+
+get_par_path <- function(model, par = 'alpha', model_name = NULL, real_par = NULL){
+  transpose(model$steps)[[par]] %>%
+    lapply(as.vector) %>% do.call(cbind, .) %>%
+    data.table() -> dt
+
+  colnames(dt) <- as.character(seq_len(ncol(dt)))
+  dt[,index := 1:.N]
+  dt <- melt(dt, id.vars = 'index', variable.name = 'step', value.name = 'parameter')
+  dt[,step := as.numeric(step)]
+  dt[,index := factor(index)]
+  if(!is.null(real_par)) dt[,real_par := rep(real_par, times = max(step))]
+  if(!is.null(model_name)) dt[,model := model_name]
+  return(dt)
+}
+
+get_full_path <- function(...){
+  dt_list <- list(...)
+  for(i in 2:length(dt_list)) dt_list[[i]][,step := step + max(dt_list[[i - 1]]$step) - 1]
+  dt_full <- do.call(rbind, dt_list)
+  return(dt_full)
+}
+
+create_plot <- function(dt, hline = NULL, size = 20){
+  if(size < 1 | size > nrow(dt)) size <- nrow(dt)
+  samp <- dt[,sort(sample(unique(index), size))]
+  p <- ggplot(dt[index %in% samp], aes(x = step, y = parameter, col = index))
+  if(!is.null(hline)) p <- p + geom_hline(yintercept = hline)
+  p <- p + geom_vline(xintercept = dt[model == 'iid', max(step)],
+                      color = 'lightgrey', linetype = 2, size = 1) +
+    geom_line() + geom_point(aes(shape = model)) +
+    theme_bw() +
+    theme(legend.position = 'none')
+  if('real_par' %in% colnames(dt)) p <- p + geom_hline(aes(yintercept = real_par, col = index), linetype = 2)
+
+  return(p)
+}
+
+get_emp_cov <- function(dt, model){
+  expected_dt <- rep(1, nrow(dt)) %o% triangle2vector(with(model, linkFun$FUN(theta, alpha, 1)))
+  residuals_ <- dt - expected_dt
+  emp_cov <- t(residuals_) %*% residuals_
+
+  return(emp_cov)
+}
+
+minimize_cov_distance <- function(data, model, reg_method = 'increase_diag'){
+  reg_method <- match.arg(reg_method, c('avg_diag', 'increase_diag'), F)
+  dt <- convert_corr_array_to_data_matrix_test(data)
+  emp_cov <- get_emp_cov(dt, model)
+  g12 <- corrmat_covariance_from_dt(dt)
+
+  optim_fun <- function(par, reg_method) sum(triangle2vector(
+    regularize_matrix(g12, reg_method, par, F) - emp_cov
+  )^2)
+
+  out <- optimise(optim_fun, 0:1, reg_method = reg_method)
+  return(out)
+}
+
 create_estimates <- function(n_sim, n, p, percent_alpha, range_alpha, ARMA = 0, verbose = FALSE){
   # no arma, null case
   case <- if(percent_alpha == 0) 'No Effect' else 'Effect'
@@ -62,7 +126,6 @@ create_variance_estimates <- function(n_sim, n, p, p_s, percent_alpha, range_alp
   out[,`:=`(autocorrelated = autocorrelated, p_s = p_s, case = case)]
   return(out)
 }
-
 
 create_sample_estimates <- function(n_sim, n, p, p_s, percent_alpha, range_alpha, ARMA = 0, ncores = 1, sim = NULL){
   case <- if(percent_alpha == 0) 'No Effect' else "Effect"
---
+++ tex/simulations/compare_bias.R
@@ -0,0 +1,6 @@
+source("tex/simulations/aux_.R")
+
+#  todo: estimate OLS, Regularized, and Full on Random/Non Random Effect (total 6)
+#  check best estimators MSE and Bias; Simulate with real TGA Theta
+
+

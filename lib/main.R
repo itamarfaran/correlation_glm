efe3ef743322cb294f4d5e9fabe5550e8da0120b
itamarf <itamar.faran@gmail.com>
2020-09-25 15:46:52 +0300

    refactor main

--- lib/main.R
+++ lib/main.R
@@ -4,8 +4,11 @@ source('lib/utils/simulation_functions.R')
 source('lib/model/estimation_functions.R')
 source('lib/model/inference_functions.R')
 
+
+##### simulation pars #####
+
 linkFun <- linkFunctions$multiplicative_identity
-p <- 240
+p <- 32
 n_h <- 10
 n_s <- 10
 T_thresh <- Tlength <- 115
@@ -16,36 +19,70 @@ ARMAdetails <- list(
 )
 sapply(ARMAdetails, check_invertability_arma)
 
-#sample_data <- create_samples(
-#  n_h = n_h, n_s = n_s, p = p, Tlength = Tlength, dim_alpha = 1,
-#  percent_alpha = 0.3, range_alpha = c(0.9, 0.9),
-#  ARsick = ARMAdetails$ARsick, MAsick = ARMAdetails$MAsick,
-#  ARhealth = ARMAdetails$ARhealth, MAhealth = ARMAdetails$MAhealth,
-#  ncores = ncores
-#  )
 
- sample_data <- prepare_corrmat_data(
-   subset = 1:p,
-   healthy_index_name = 'CONTROLS',
+##### data analysis configs pars #####
 
-#  link = 'lib/data/Amnesia_all_AAL.mat',
-   link = 'lib/data/Amnesia_all_AICHA.mat',
-   corr_matrix_name = 'corrmats',
-   sick_index_name = 'TGA'
+data_configs <- list(
+  amnesia_aal = list(
+    link = 'lib/data/Amnesia_all_AAL.mat',
+    corr_matrix_name = 'corrmats',
+    healthy_index_name = 'CONTROLS',
+    sick_index_name = 'TGA'
+  ),
+  amnesia_aicha = list(
+    link = 'lib/data/Amnesia_all_AICHA.mat',
+    corr_matrix_name = 'corrmats',
+    healthy_index_name = 'CONTROLS',
+    sick_index_name = 'TGA'
+  ),
+  adni_aal = list(
+    link = 'lib/data/ADNI_data_AD_CN.mat',
+    corr_matrix_name = 'all.corrmats',
+    healthy_index_name = 'CONTROLS',
+    sick_index_name = 'AD'
+  ),
+  nmda_aal = list(
+    link = 'lib/data/NMDA_all_data_AAL90.mat',
+    corr_matrix_name = 'group.all',
+    healthy_index_name = 'CONTROLS',
+    sick_index_name = 'NMDA'
+  )
+)
 
-#   link = 'lib/data/ADNI_data_AD_CN.mat',
-#   corr_matrix_name = 'all.corrmats',
-#   sick_index_name = 'AD'
 
-#   link = 'lib/data/NMDA_all_data_AAL90.mat',
-#   corr_matrix_name = 'group.all',
-#   sick_index_name = 'NMDA'
- )
+##### data analysis pars #####
+
+data_conf <- NULL
+data_subset <- NULL
+
+if(is.null(data_conf)){
+  sample_data <- create_samples(
+    n_h = n_h, n_s = n_s, p = p, Tlength = Tlength, dim_alpha = 1,
+    percent_alpha = 0.3, range_alpha = c(0.9, 0.9),
+    ARsick = ARMAdetails$ARsick, MAsick = ARMAdetails$MAsick,
+    ARhealth = ARMAdetails$ARhealth, MAhealth = ARMAdetails$MAhealth,
+    ncores = ncores
+  )
+} else {
+  sample_data <- prepare_corrmat_data(
+    link = data_conf$link,
+    corr_matrix_name = data_conf$corr_matrix_name,
+    healthy_index_name = data_conf$healthy_index_name,
+    sick_index_name = data_conf$sick_index_name,
+    subset = if(is.null(data_subset)) NULL else 1:data_subset
+  )
+}
+
+##### analysis #####
 
 test_corr_mat(sample_data)
 
-results <- estimate_alpha(healthy_dt = sample_data$samples$healthy, sick_dt = sample_data$samples$sick,
-                          early_stop = TRUE, matrix_reg_config = list(method = 'constant', const = 0.1))
+results <- estimate_alpha(
+  healthy_dt = sample_data$samples$healthy,
+  sick_dt = sample_data$samples$sick,
+  early_stop = FALSE,
+  matrix_reg_config = list(method = 'constant', const = 0.1)
+  )
 
 gee_var <- compute_gee_variance(
   healthy_dt = sample_data$samples$healthy,

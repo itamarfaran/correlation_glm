3a0ba7f6e1789c430cbb0de2d110b71ece2b61c1
itamarf <itamar.faran@gmail.com>
2020-11-21 19:10:49 +0200

    simplify calcs

--- lib/model/inference_functions.R
+++ lib/model/inference_functions.R
@@ -119,17 +119,16 @@ infer_jacknife <- function(results){
   n_h <- sum(!sick_ind)
   
   estimate_d <- colMeans(results$alpha[sick_ind,])
-  const_d <- (n_s - 1)^2  # /n_s
+  const_d <- (n_s - 1)^2/n_s
   var_d <- var(results$alpha[sick_ind,])*const_d
 
   estimate_h <- colMeans(results$alpha[!sick_ind,])
-  const_h <- (n_h - 1)^2  # /n_h
+  const_h <- (n_h - 1)^2/n_h
   var_h <- var(results$alpha[!sick_ind,])*const_h
   
   estimate <- colMeans(results$alpha)
   # estimate <- (estimate_d + estimate_h)/2
-  # var_out <- var_d + var_h
-  var_out <- var_d/n_s + var_h/n_h
+  var_out <- var_d + var_h
   
   return(list(
     estimate = estimate,

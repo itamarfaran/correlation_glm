32a6598456381ccf0f0e4f6fd4c205bd7be8411f
itamarf <itamar.faran@gmail.com>
2020-11-21 19:02:31 +0200

    add sims to jackknife

--- tex/simulations/jacknife.R
+++ tex/simulations/jacknife.R
@@ -1,42 +1,53 @@
 source('tex/simulations/aux_.R')
 
-n_sim <- 100
+n_repeat <- 5
+n_sim <- ncores
 n <- 50
-p <- 24
+p <- 32
 range_alpha <- c(0.7, 1.1)
 percent_alpha <- 0.8
 ARMA <- 0.5
 
 n_h <- ceiling(0.5*n)
 n_s <- n - n_h
-samples <- create_samples(
-  n_sim = n_sim, n_h = n_h, n_s = n_s, p = p, 
-  percent_alpha = percent_alpha, range_alpha = range_alpha, 
-  ARsick = ARMA, ARhealth = ARMA, MAsick = ARMA, MAhealth = ARMA,
-  ncores = ncores
-)
 
-estimates <- do.call(rbind, pbmclapply(1:n_sim, function(i){
-  out <- estimate_alpha(
-    healthy_dt = samples$samples[[i]]$healthy,
-    sick_dt = samples$samples[[i]]$sick,
-    verbose = FALSE)
-  return(as.vector(out$alpha))
-}, mc.cores = ncores))
+results <- list()
 
-jackknife_estimates <- estimate_alpha_jacknife(
-  healthy_dt = samples$samples[[1]]$healthy,
-  sick_dt = samples$samples[[1]]$sick,
-  verbose = TRUE, ncores = ncores
-)
+for(i in seq_len(n_repeat)){
+  samples <- create_samples(
+    n_sim = n_sim, n_h = n_h, n_s = n_s, p = p, 
+    percent_alpha = percent_alpha, range_alpha = range_alpha, 
+    ARsick = ARMA, ARhealth = ARMA, MAsick = ARMA, MAhealth = ARMA,
+    ncores = ncores
+  )
+  
+  estimates <- do.call(rbind, pbmclapply(1:n_sim, function(i){
+    out <- estimate_alpha(
+      healthy_dt = samples$samples[[i]]$healthy,
+      sick_dt = samples$samples[[i]]$sick,
+      verbose = FALSE)
+    return(as.vector(out$alpha))
+  }, mc.cores = ncores))
+  
+  jackknife_estimates <- estimate_alpha_jacknife(
+    healthy_dt = samples$samples[[1]]$healthy,
+    sick_dt = samples$samples[[1]]$sick,
+    verbose = TRUE, ncores = ncores
+  )
+  
+  jackknife_inference <- infer_jacknife(jackknife_estimates)
+  
+  
+  sds_dt <- data.table(
+    sim_num = i,
+    jackknife = sqrt_diag(jackknife_inference$variance),
+    empiric = sqrt_diag(var(estimates))
+  )
+  
+  results[[i]] <- sds_dt
+}
 
-jackknife_inference <- infer_jacknife(jackknife_estimates)
-
-
-sds_dt <- data.table(
-  jackknife = sqrt_diag(jackknife_inference$variance),
-  empiric = sqrt_diag(var(estimates))
-)
+sds_dt <- do.call(rbind, results)
 
 plt <- ggplot(sds_dt, aes(x = empiric, y = jackknife)) + 
   geom_point(shape = 21, col = 'black', fill = 'darkgrey', alpha = 0.7) + 

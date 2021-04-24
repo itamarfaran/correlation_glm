2d4463b696e7efda8582113d5537eb685662baab
itamarf <itamar.faran@gmail.com>
2021-04-24 11:58:18 +0300

    add devtools to requirements

--- lib/utils/ipak_and_prompt_for_cores.R
+++ lib/utils/ipak_and_prompt_for_cores.R
@@ -5,7 +5,7 @@ ipak <- function(..., only_install = FALSE){
   if(!only_install) sapply(pkg, require, character.only = TRUE)
 }
 
-packages <- c("abind", "corrplot", "data.table", "Matrix", "matrixcalc",
+packages <- c("abind", "corrplot", "data.table", "devtools", "Matrix", "matrixcalc",
               "mvtnorm", "numDeriv", "parallel", "plotly", "profvis", "progress",
               "Rcpp", "R.matlab", "stats4", "tidyverse", "GGally", "pbmcapply", "pbapply")
 ipak(packages)
